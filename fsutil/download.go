96036c0da336944af9dd9bdd6f9e1ba17dd3af7f
luoyu <luoyu@medlinker.com>
2023-06-28 23:04:15 +0800

    feat: [fsutil] mod remote file download

--- fsutil/download.go
+++ fsutil/download.go
@@ -3,15 +3,21 @@ package fsutil
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
+	"net/url"
 	"os"
+	"path"
 )
 
 // Download 下载远程文件
-func Download(filename, url string) error {
-	// todo 检查url
+func Download(filename, remoteUrl string) error {
+	// 检查url
+	if _, err := url.Parse(remoteUrl); nil != err {
+		return err
+	}
 
-	resp, err := http.Get(url)
+	resp, err := http.Get(remoteUrl)
 	if err != nil {
 		return err
 	}
@@ -20,8 +26,11 @@ func Download(filename, url string) error {
 	}()
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return fmt.Errorf("remote response failed: status code = %d", resp.StatusCode)
+		return fmt.Errorf("remote response failed, status code = %d", resp.StatusCode)
 	}
+
+	// 创建文件目录
+	_ = os.MkdirAll(path.Dir(filename), fs.ModePerm)
 
 	// 创建文件
 	out, err := os.Create(filename)
